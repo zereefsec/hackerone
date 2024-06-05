ea9e6458b0df83e1c0e556d614780354a67f4cad
zereefsec <108139112+zereefsec@users.noreply.github.com>
2024-06-05 14:25:46 +0530

    Create h1_scope.sh

---
+++ h1_scope.sh
@@ -0,0 +1,7 @@
+python programs.py
+python scope.py programs.json
+python inscope.py scope.json
+python bounty.py inscope.json bounty/bounty.json
+python nobounty.py inscope.json nobounty/nobounty.json
+chmod +x bounty/*
+chmod +x nobounty/*
