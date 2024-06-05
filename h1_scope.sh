ef570d2e14b1696e2f2c1cd302780a9001f17927
zereefsec <108139112+zereefsec@users.noreply.github.com>
2024-06-05 16:17:07 +0530

    Update h1_scope.sh

--- h1_scope.sh
+++ h1_scope.sh
@@ -1,7 +1,7 @@
-python programs.py
-python scope.py programs.json
-python inscope.py scope.json
-python bounty.py inscope.json bounty/bounty.json
-python nobounty.py inscope.json nobounty/nobounty.json
+python3 programs.py
+python3 scope.py programs.json
+python3 inscope.py scope.json
+python3 bounty.py inscope.json bounty/bounty.json
+python3 nobounty.py inscope.json nobounty/nobounty.json
 chmod +x bounty/*
 chmod +x nobounty/*
