db42a42387e6ecd490430ff4ca0d7ce21663cc22
zereefsec <108139112+zereefsec@users.noreply.github.com>
2024-06-05 16:17:24 +0530

    Update bountyscan.sh

--- bounty/bountyscan.sh
+++ bounty/bountyscan.sh
@@ -1,4 +1,4 @@
-python bounty_domain.py bounty.json bounty_domain.json bounty_domain.txt WILDCARD URL OTHER
+python3 bounty_domain.py bounty.json bounty_domain.json bounty_domain.txt WILDCARD URL OTHER
 cat bounty_domain.txt | anew domains.txt
 subfinder -dL domains.txt -all -o subdomains.txt
 httpx -l domains.txt -p 80,443,8080,8443,8000,8888,481,15002,8006,9443,9982 -t 200 -o httpx1.txt -title -wc -sc cl -ct -location -web-server -asn -nf
