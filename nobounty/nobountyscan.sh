a8a2b19e657e74899f8e8842be0a18eac11d38fa
zereefsec <108139112+zereefsec@users.noreply.github.com>
2024-06-05 16:17:41 +0530

    Update nobountyscan.sh

--- nobounty/nobountyscan.sh
+++ nobounty/nobountyscan.sh
@@ -1,4 +1,4 @@
-python nobounty_domain.py nobounty.json nobounty_domain.json nobounty_domain.txt WILDCARD URL OTHER
+python3 nobounty_domain.py nobounty.json nobounty_domain.json nobounty_domain.txt WILDCARD URL OTHER
 cat nobounty_domain.txt | anew domains.txt
 subfinder -dL domains.txt -all -o subdomains.txt
 httpx -l domains.txt -p 80,443,8080,8443,8000,8888,481,15002,8006,9443,9982 -t 200 -o httpx1.txt -title -wc -sc cl -ct -location -web-server -asn -nf
